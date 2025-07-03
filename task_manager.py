b3929bf2613e468e808fcf289837a542625e416c
Prachi Aggarwal <prachi.basant.aggarwal@gmail.com>
2025-07-03 17:19:14 +0530

    Add files via upload

---
+++ task_manager.py
@@ -0,0 +1,71 @@
+import json
+
+def save():
+    with open ("tasks.json", "w") as file:
+        json.dump(tasks,file)
+    
+def load():
+    global tasks
+    try:
+        with open("tasks.json", "r") as file:
+            tasks = json.load(file)
+    except FileNotFoundError:
+        tasks = []
+
+load()
+
+def menu():
+    print("Task manager📝")
+    print("1. View Tasks")
+    print("2. Add Tasks")
+    print("3. Mark as complete")
+    print("4. Delete Task")
+    print("5. Exit")
+
+def view():
+    if not tasks:
+        print("No tasks yet!")
+    for i, task in enumerate(tasks, start=1):
+        status = "✔️" if task['done'] else "❌"
+        print(f"{i}. {task['title']} - [{status}]")
+
+def add():
+    new = input("Enter the task: ")
+    task = {"title": new, "done": False}
+    tasks.append(task)
+    save()
+    print("Task added✔️")
+
+def mark():
+    view()
+    try:
+        no = int(input("Enter the number of task to be marked done: "))
+        tasks[no-1]["done"] = True
+        save()
+        print("Task marked as done!")
+    except:
+        print("Invalid task number!")
+
+def delete():
+    view()
+    tsk = int(input("Enter the number task to be deleted: "))
+    tasks.pop(tsk-1)
+    save()
+    print("Task deleted!🚮")
+
+while True:
+    menu()
+    choice = int(input("Choose an option from 1 to 5: "))
+    if choice == 1:
+        view()
+    elif choice == 2:
+        add()
+    elif choice == 3:
+        mark()
+    elif choice == 4:
+        delete()
+    elif choice == 5:
+        print("Exiting. Bye :)")
+        break
+    else:
+        print("Invalid choice.")
